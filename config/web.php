34ce8212ac32b804faa2f326b36cf6d2dbb3247c
dv4mp1r3 <admin@mainframe>
2018-12-23 19:32:04 +0300

    добавлен модуль для рендера markdown обновлены ридми добавлена документация (в общих чертах) фикс cookieValidationKey в конфиге

--- config/web.php
+++ config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => require_once './cookieValidationKey.php',
+            'cookieValidationKey' => require_once __DIR__.'/cookieValidationKey.php',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -46,6 +46,11 @@ $config = [
             ],
         ],
         */
+    ],
+    'modules' => [
+        'markdown' => [
+            'class' => 'kartik\markdown\Module',
+        ],
     ],
     'params' => $params,
 ];
--- views/site/about.php
+++ views/site/about.php
@@ -3,16 +3,12 @@
 /* @var $this yii\web\View */
 
 use yii\helpers\Html;
+use kartik\markdown\Markdown;
 
 $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        This is the About page. You may modify the following file to customize its content:
-    </p>
-
-    <code><?= __FILE__ ?></code>
+    <?= Markdown::convert(file_get_contents(Yii::$app->getBasePath().'/assets/md/readme.md')); ?>
 </div>
